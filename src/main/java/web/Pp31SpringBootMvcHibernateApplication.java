896533da24604a32988669816967a16316852e08
Ivn665 <157416939+Ivn665@users.noreply.github.com>
2024-06-24 22:21:09 +0300

    Мелкие доработки

--- src/main/java/web/Pp31SpringBootMvcHibernateApplication.java
+++ src/main/java/web/Pp31SpringBootMvcHibernateApplication.java
@@ -10,12 +10,13 @@ public class Pp31SpringBootMvcHibernateApplication {
 
     public static void main(String[] args){
         SpringApplication.run(Pp31SpringBootMvcHibernateApplication.class, args);
-/*        System.out.println("Started");
+        System.out.println("Started");
+
         try {
             openHomePage();
         } catch (IOException e) {
             System.out.println("Не удалось автоматически открыть стартовую страницу в браузере");
-        }*/
+        }
     }
     private static void openHomePage() throws IOException {
         Runtime rt = Runtime.getRuntime();
